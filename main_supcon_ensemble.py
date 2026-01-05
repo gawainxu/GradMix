9ea93eb3d36615fcd38447a2732f330b4f3446cf
gawainxu <xgawain@gmail.com>
2026-01-05 18:22:25 +0100

    py script for ensemble training

--- main_supcon_ensemble.py
+++ main_supcon_ensemble.py
@@ -416,7 +416,7 @@ def main():
         if epoch % opt.save_freq == 0:
             save_file = os.path.join(
                 opt.save_folder, 'ckpt_epoch_{epoch}.pth'.format(epoch=epoch))
-            save_model(model, optimizer, opt, epoch, save_file)
+            save_model(model, None, optimizer, opt, epoch, save_file)
 
     # save the last model
     save_file = os.path.join(
