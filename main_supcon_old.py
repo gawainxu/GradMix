0631c47af00f84d8d38a01721ac354ce5397e662
gawainxu <xgawain@gmail.com>
2026-03-26 19:57:13 +0100

    ultrasound dataset

--- main_supcon_old.py
+++ main_supcon_old.py
@@ -362,7 +362,7 @@ def train(train_loader, model, linear, criterion1, criterion2, optimizer, epoch,
 
     for idx, (images, labels, annotations) in enumerate(train_loader):
 
-        #print("images", images[0].shape, len(annotations))
+        print("images", images[0].shape, len(annotations))
 
         data_time.update(time.time() - end)
         images1 = images[0]
