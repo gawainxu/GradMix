cd2ea8f45c21efb184af5e04bfa38933e2dd429a
gawainxu <xgawain@gmail.com>
2026-03-27 21:45:47 +0100

    test dino added

--- test_dino.py
+++ test_dino.py
@@ -4,6 +4,7 @@ import numpy as np
 import argparse
 
 from  datautil import get_train_datasets, get_test_datasets, get_outlier_datasets
+from feature_linear import set_model, train, test, features_set
 
 
 def parse_option():
@@ -21,6 +22,11 @@ def parse_option():
                                  "feature_reading"])
     parser.add_argument("--trail", type=int, default=0, choices=[0, 1, 2, 3, 4, 5, 6],
                         help="index of repeating training")
+    parser.add_argument("--test_mode", type=str, default="linear", choices=["linear", "cosine", "knn"])
+    parser.add_argument("--num_classes", type=int, default=3)
+    parser.add_argument("--linear_input_dim", type=int, default=128)
+    parser.add_argument("--learning_rate", type=float, default=0.01)
+    parser.add_argument("--epochs", type=int, default=5)
 
     opt = parser.parse_args()
 
@@ -66,7 +72,7 @@ def read_dino_features(test_loader, model):
         img = img.repeat(1, 3, 1, 1)
         img = img.cuda() if torch.cuda.is_available() else img
         f = model(img)
-        features.append(torch.squeeze(f.cup()))
+        features.append(torch.squeeze(f.cpu()))
         labels.append(torch.squeeze(label.cpu()))
 
     return features, labels
@@ -85,14 +91,19 @@ def sort_features(mixedFeatures, labels, num_classes):
     return sortedFeatures
 
 
-def classification_evaluation(sorted_train_features, test_features, test_label):
 
+def linear_probe(train_features, train_labels, test_features, test_labels, opt):
 
-    return 0
-
-
-
-
+    train_set = features_set(train_features, train_labels)
+    test_set = features_set(test_features, test_labels)
+    train_dataloader = torch.utils.data.DataLoader(train_set, batch_size=64, shuffle=True,
+                                                   num_workers=1, pin_memory=True)
+    test_dataloader = torch.utils.data.DataLoader(test_set, batch_size=1, shuffle=False,
+                                                  num_workers=1, pin_memory=True)
+    classifier, criterion, optimizer = set_model(opt)
+    losses, top1, top5 = train(classifier, criterion, optimizer, train_dataloader, opt)
+    test_acc = test(classifier, test_dataloader, opt)
+    print("original testing accuracy", test_acc)
 
 
 if __name__ == "__main__":
@@ -106,6 +117,8 @@ if __name__ == "__main__":
 
     test_features, test_labels = read_dino_features(test_loader, model)
     outlier_features, _ = read_dino_features(outlier_loader, model)
+
+    linear_probe(train_features, train_labels, test_features, test_labels, opt)
 
 
 
