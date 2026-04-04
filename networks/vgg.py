acf49f3a62d8e6f933f43d1f42abacb073344f47
gawainxu <xgawain@gmail.com>
2026-04-04 19:42:23 +0200

    vgg added

--- networks/vgg.py
+++ networks/vgg.py
@@ -65,9 +65,8 @@ class VGG(nn.Module):
     def __init__(self, features, num_classes=1000, init_weights=True):
         super(VGG, self).__init__()
         self.features = features
-
-        """
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        """
         self.classifier = nn.Sequential(
             nn.Linear(512 * 7 * 7, 4096),
             nn.ReLU(True),
@@ -77,9 +76,9 @@ class VGG(nn.Module):
             nn.Dropout(),
             nn.Linear(4096, num_classes),
         )
+        """
         if init_weights:
             self._initialize_weights()
-        """
 
     def forward(self, x):
         x = self.features(x)
