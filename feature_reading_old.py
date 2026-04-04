5adbb9969aec4f4426a3da41aa315896287fcf21
gawainxu <xgawain@gmail.com>
2026-04-04 21:58:21 +0200

    vgg added

--- feature_reading_old.py
+++ feature_reading_old.py
@@ -190,7 +190,7 @@ def normalFeatureReading(data_loader, model, linear_model, opt):
     outputs_linear = []
     labels = []
 
-    for i, (img, label, _) in enumerate(data_loader):
+    for i, (img, label) in enumerate(data_loader):
         
         print(i)
         if i > opt.break_idx:
