fd40c4c6bbf0a92ec9533d5d2c6a6b167231c656
gawainxu <xgawain@gmail.com>
2026-08-02 22:00:56 +0200

    gradmix at supcon side

--- feature_specturm.py
+++ feature_specturm.py
@@ -77,8 +77,10 @@ if __name__ == "__main__":
             sorted_features_head = sortFeatures(features_head2, labels2, opt.num_classes)
             sorted_features_backbone = sortFeatures(features_backbone2, labels2, opt.num_classes)
 
-    cka_linear = linear_cka_gpt(features_head1, features_head2)
-    print("cka is", cka_linear)
+    cka_head = linear_cka_gpt(features_head1, features_head2)
+    print("cka is", cka_head)
+    cka_backbone = linear_cka_gpt(features_head1, features_head2)
+    print("cka is", cka_backbone)
 
     """
     if opt.mode == "intra":
