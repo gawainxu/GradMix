130922b064dbde6fb4acc1cb86e4bbefcf309ea5
gawainxu <xgawain@gmail.com>
2026-08-04 12:42:08 +0200

    gradmix at supcon side

--- feature_reading_old.py
+++ feature_reading_old.py
@@ -255,7 +255,7 @@ if __name__ == "__main__":
     
     featurePaths= []
 
-    if opt.if_train == "train" or opt.if_train == "test_known" or opt.if_train == "full":
+    if opt.if_train == "train" or opt.if_train == "full":   #opt.if_train == "test_known" or 
         for r in range(0, opt.n_cls):
             opt.save_path = opt.feature_save + "temp" + str(r)
             featurePaths.append(opt.save_path)
